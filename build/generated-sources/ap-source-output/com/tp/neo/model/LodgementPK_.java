7877c3c51d5e653081af37e931456488baf99f6b
Techie Planet <techieplanet@gmail.com>
2016-09-17 09:59:21 +0100

    Refactored the ProjectUnit class, removed the embedded projectunitpk

--- build/generated-sources/ap-source-output/com/tp/neo/model/LodgementPK_.java
+++ build/generated-sources/ap-source-output/com/tp/neo/model/LodgementPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-14T22:02:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-16T21:32:37")
 @StaticMetamodel(LodgementPK.class)
 public class LodgementPK_ { 
 
--- build/generated-sources/ap-source-output/com/tp/neo/model/Project_.java
+++ build/generated-sources/ap-source-output/com/tp/neo/model/Project_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-14T22:02:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-16T21:32:37")
 @StaticMetamodel(Project.class)
 public class Project_ { 
 
@@ -21,8 +21,8 @@ public class Project_ {
     public static volatile SingularAttribute<Project, Long> createdBy;
     public static volatile SingularAttribute<Project, String> name;
     public static volatile SingularAttribute<Project, Date> modifiedDate;
-    public static volatile SingularAttribute<Project, String> location;
     public static volatile SingularAttribute<Project, Long> modifiedBy;
+    public static volatile SingularAttribute<Project, String> location;
     public static volatile SingularAttribute<Project, Integer> id;
 
 }
